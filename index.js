f95a548d8cae667e89e0b8ce0101438040181ab1
aoko-code <cynthiakhamenge@yahoo.com>
2022-07-28 08:59:22 +0300

    done

---
+++ index.js
@@ -0,0 +1,31 @@
+function bankAccount(accNo, accName, balance) {
+    this.accNo = accNo;
+    this.accName = accName;
+    this.balance = balance;
+}
+
+bankAccount.prototype.deposit = function (dAmount) {
+    if (isNaN(parseFloat(dAmount))) {
+        return console.log('please enter the amount in numbers')
+    } else if (Math.sign(dAmount) === -1) {
+        return console.log('please enter a positive number')
+    } else {
+        return console.log(`you have deposited ${dAmount} and this is your balance ${this.balance += dAmount}`)
+    }
+}
+
+bankAccount.prototype.withdraw = function (wAmount) {
+    if (wAmount > this.balance) {
+        return console.log(`you do not have sufficient balance in your account. Your account balance is ${this.balance}`)
+    } else {
+        return console.log(`you have withdrawn ${wAmount} and this is your balance ${this.balance -= wAmount}`)
+    }
+}
+bankAccount.prototype.getBalance = function () {
+    return console.log(`your balance is ${this.balance}`)
+}
+
+user1 = new bankAccount(001, 'jane', 10000)
+console.log(user1.deposit(5000))
+console.log(user1.withdraw(20000))
+console.log(user1.getBalance())
